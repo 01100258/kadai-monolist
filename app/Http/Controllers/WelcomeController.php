dcaa2eed5dde42104772ef65b62e10de97baf29f
jiro.kirameki <qualial2014@gmail.com>
2018-04-07 11:50:17 +0000

    Procfile2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,18 +11,12 @@ use App\Item;
 
 class WelcomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         $items = Item::orderBy('updated_at', 'desc')->paginate(20);
         return view('welcome', [
             'items' => $items,
         ]);
-        
-        
     }
 }
