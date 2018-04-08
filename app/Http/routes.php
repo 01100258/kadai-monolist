8ad7f040feb3ea97251294ad5f81bd0a82109020
jiro.kirameki <qualial2014@gmail.com>
2018-04-08 10:29:04 +0000

    ranking2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,9 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
 
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
+
 Route::group(['middleware' => 'auth'], function () {
         Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('have', 'ItemUserController@have')->name('item_user.have');
